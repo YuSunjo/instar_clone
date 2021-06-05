42595fa6d1b1564ad9bd141ac3b9855dc31b5b51
YuSunjo <tnswh2023@gmail.com>
2021-06-05 18:02:35 +0900

    feat(story) : 스토리 페이징

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.domain.image;
 
 import com.cos.photogramstart.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,8 +24,9 @@ public class Image {
     private String caption;
     private String postImageUrl;   // 사진 전송 받아서 그 사진을 서버에 특정 폴더에 저장 - db에 그 저장된 경로
 
+    @JsonIgnoreProperties({"images"})
     @JoinColumn(name = "userId")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
     // 이미지 좋아요
--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -6,11 +6,15 @@ import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,6 +26,7 @@ public class ImageService {
     @Value("${file.path}")
     private String uploadFolder;
 
+    @Transactional
     public void imageUpload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
         UUID uuid = UUID.randomUUID();
         String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename();
@@ -38,7 +43,12 @@ public class ImageService {
         // image 테이블에 저장
         Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser());
         imageRepository.save(image);
+    }
 
+    @Transactional(readOnly = true)
+    public Page<Image> imageStory(Long principalId, Pageable pageable) {
+        Page<Image> images = imageRepository.mStory(principalId, pageable);
+        return images;
     }
 
 }
